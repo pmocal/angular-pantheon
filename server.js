0f4668f62ba15e115761ab1e55393c6759a3e91e
Parthiv Mohan <parthiv.mohan@gmail.com>
2019-07-18 19:58:52 -0400

    youknow

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const port = process.env.PORT || 8080;
 var app = express();
 
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/dist/angular-tour-of-heroes"));
 
 // send the user to index html page in spite of the url
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname + '/src', 'index.html'));
+  res.sendFile(path.resolve(__dirname + "/dist/angular-tour-of-heroes/index.html"));
 });
 
 app.listen(port);
