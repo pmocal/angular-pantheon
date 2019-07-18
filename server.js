d3685b3c4e8d9c6a5cbbb807e3db1fec6ed82af6
Parthiv Mohan <parthiv.mohan@gmail.com>
2019-07-18 19:49:06 -0400

    angular

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080;
 var app = express();
 
 // the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname));
 
 // send the user to index html page in spite of the url
 app.get('*', (req, res) => {
